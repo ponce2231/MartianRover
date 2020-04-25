b6eb54a476e73c3e2267070c0aa75f86e0c3baf6
Christopher <christopherponce@hotmail.com>
2020-04-25 10:46:50 -0400

    first commit

---
+++ MartianRover/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MartianRover
+//
+//  Created by Christopher Ponce Mendez on 4/25/20.
+//  Copyright © 2020 Christopher Ponce Mendez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-MartianRover/Pods-MartianRover-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MartianRover : NSObject
+@end
+@implementation PodsDummy_Pods_MartianRover
+@end
